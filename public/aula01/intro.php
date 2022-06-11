08f34da49c8e619c5f9c933008a0e66575bec91b
israelcena <israelsjm@gmail.com>
2022-06-11 20:57:42 -0300

    update aula01

--- public/aula01/intro.php
+++ public/aula01/intro.php
@@ -1,10 +1,18 @@
 <?php
 
 include "./Aluno.php";
+include "./Curso.php";
+include "./Disciplina.php";
+include "./Professor.php";
 
 $a1 = new Aluno();
-
 $a1->nome = "Davi";
 $a1->cpf = "321321321";
+$a1->nome .= " Nunes";
 
-var_dump($a1);
+$cursoPHP = new Curso();
+$cursoPHP->nome = "Introduçao ao PHP";
+$cursoPHP->cargaHoraria = 90;
+$cursoPHP->descricao = "Curso para aprender o mínimo de php e menor tempo";
+
+echo "<h1>Aluno: {$a1->nome}</h1>";
