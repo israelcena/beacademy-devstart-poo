ee8abb244b24ea790697d969ec63fae304c78560
Israel Cena <israelsjm@gmail.com>
2022-06-12 18:37:41 -0300

    incluide another class

--- public/aula06/index.php
+++ public/aula06/index.php
@@ -6,11 +6,14 @@ include 'vendor/autoload.php';
 
 use Classes\Config\Usuario as UsuarioConfig;
 use Classes\Categoria as Cat;
+use Classes\Config\Falses\Falsa as FalsaConfig;
 
 $us2 = new UsuarioConfig();
 $c1 = new Cat();
+$f1 = new FalsaConfig();
 
 var_dump($us2);
 var_dump($c1);
+var_dump($f1);
 
 echo "Tudo ok";
