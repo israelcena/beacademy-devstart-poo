ed680294c4b39cfe5038395eef594126b139820d
Israel Cena <israelsjm@gmail.com>
2022-06-12 18:33:50 -0300

    include classes with autoload and use

--- public/aula06/index.php
+++ public/aula06/index.php
@@ -5,9 +5,12 @@ ini_set('display_errors', 1);
 include 'vendor/autoload.php';
 
 use Classes\Config\Usuario as UsuarioConfig;
+use Classes\Categoria as Cat;
 
 $us2 = new UsuarioConfig();
+$c1 = new Cat();
 
 var_dump($us2);
+var_dump($c1);
 
 echo "Tudo ok";
