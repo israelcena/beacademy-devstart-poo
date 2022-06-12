64581c533cf1fa234a73b2dfb112700827fb9dc9
Israel Cena <israelsjm@gmail.com>
2022-06-12 18:27:56 -0300

    incude composer auto load (dump-autoload)

--- public/aula06/index.php
+++ public/aula06/index.php
@@ -2,20 +2,12 @@
 
 ini_set('display_errors', 1);
 
-include 'classes/Categoria.php';
-include 'classes/Produto.php';
-include 'classes/Usuario.php';
-include 'classes/Cliente.php';
-include 'classes/Gestor.php';
-include 'classes/Config/Usuario.php';
+include 'vendor/autoload.php';
 
 use Classes\Config\Usuario as UsuarioConfig;
 
-$us1 = new Classes\Usuario();
-// $us2 = new Classes\Config\Usuario();
 $us2 = new UsuarioConfig();
 
-var_dump($us1);
 var_dump($us2);
 
 echo "Tudo ok";
