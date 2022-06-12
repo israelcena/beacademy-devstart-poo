6bbd4d59fa6e25a8324325af1b164c2cdf23f911
Israel Cena <israelsjm@gmail.com>
2022-06-12 17:16:18 -0300

    include imports auto with composer

---
+++ public/aula06/index.php
@@ -0,0 +1,13 @@
+<?php
+
+ini_set('display_errors', 1);
+
+// declare(strict_types=1);
+
+include 'classes/Categoria.php';
+include 'classes/Produto.php';
+include 'classes/Usuario.php';
+include 'classes/Cliente.php';
+include 'classes/Gestor.php';
+
+echo "Tudo ok";
