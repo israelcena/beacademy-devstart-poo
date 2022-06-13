d3936c53e44aafe2ec2df8483e837b972fbb6c94
Israel Cena <israelsjm@gmail.com>
2022-06-12 23:15:00 -0300

    create not found controller and view

--- public/projeto/public/index.php
+++ public/projeto/public/index.php
@@ -2,9 +2,16 @@
 (ini_set('display_errors', 1));
 include "../vendor/autoload.php";
 
+use App\Controller\ErrorController;
 use App\Controller\IndexController;
 use App\Controller\ProductController;
-use App\Controller\CategoryController;
+// use App\Controller\CategoryController;
+
+$url = explode("?", $_SERVER['REQUEST_URI'])[0];
+echo $url;
+
+$pe = new ErrorController();
+$pe->notFoundAction();
 
 $i = new IndexController();
 $i->indexAction();
---
+++ public/projeto/src/View/error/notFound.php
@@ -0,0 +1,1 @@
+<h1>Página Não encontrada</h1>
